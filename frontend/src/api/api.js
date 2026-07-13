95be0940454a4955187efec449f06a5ba3cb909a
cpai-07 <chinmaypai657@gmail.com>
2026-07-13 14:18:21 +0100

    setting up the candidate routes endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,3 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api";
+
+// candidates
+export const getCandidates = (params) =>
+  axios.get(`${BASE_URL}/candidates`, { params });
+export const addCandidate = (data) =>
+  axios.post(`${BASE_URL}/candidates`, data);
+export const updateCandidate = (id, data) =>
+  axios.put(`${BASE_URL}/candidates/${id}`, data);
+export const deleteCandidate = (id) =>
+  axios.delete(`${BASE_URL}/candidates/${id}`);
