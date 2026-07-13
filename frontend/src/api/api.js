5f08b027ebce3b945cae83d0e245f54b516b5910
cpai-07 <chinmaypai657@gmail.com>
2026-07-13 21:18:44 +0100

    changing URL to deploy it on cloud: added external IP

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+// commenting this to deploy it on cloud(testing purpose)
+// const BASE_URL = "http://localhost:5000/api";
+
+// changing URL to deploy it on cloud
+const BASE_URL = "http://34.51.156.66:5000/api";
 
 // candidates
 export const getCandidates = (params) =>
