95b8933b9ebcceab139eed5b006c5946eb33aaf9
cpai-07 <chinmaypai657@gmail.com>
2026-07-13 13:57:16 +0100

    side bar functionality with lucide react library

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -15,6 +15,25 @@ const NAV_ITEMS = [
   { label: "Placements", icon: CheckSquare, page: "placements" },
 ];
 
-export default function Sidebar() {
-  return <div>Sidebar</div>;
+export default function Sidebar({ activePage, setActivePage }) {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo">
+        <div className="logo-dot" />
+        <span>HireFlow</span>
+      </div>
+
+      {/* iterating to each object to display nv bar  */}
+      {NAV_ITEMS.map(({ label, icon: Icon, page }) => (
+        <button
+          key={page}
+          className={`nav-item ${activePage === page ? "active" : ""}`}
+          onClick={() => setActivePage(page)}
+        >
+          <Icon size={17} />
+          {label}
+        </button>
+      ))}
+    </div>
+  );
 }
