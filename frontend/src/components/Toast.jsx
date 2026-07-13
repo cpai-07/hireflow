2e0950eee4d0d2ced2ccff36414ec7c8be556900
cpai-07 <chinmaypai657@gmail.com>
2026-07-13 14:00:50 +0100

    toast with opening and closing logic

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -1,3 +1,13 @@
-export default function Toast() {
-  return <div>Toast</div>;
+import { useEffect } from "react";
+
+export default function Toast({ message, onClose }) {
+  useEffect(() => {
+    // show popup only for 5 seconds
+    const timer = setTimeout(onClose, 5000);
+
+    // clean up function
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  return <div className="toast">{message}</div>;
 }
